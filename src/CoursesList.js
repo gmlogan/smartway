fb78d35aeb0728dba7cdc1b0ed6194dc1679a48c
Graham LOgan <graham@GRAHAMs-MacBook-Air.local>
2020-10-01 10:55:08 +0100

    Lesson 5 Far Arrow, object destructuring

--- src/CoursesList.js
+++ src/CoursesList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function CoursesList(props) {
-
-    return (
-        props.courses.map(function (course) {
-            return (
-                <div key={course.id}>
+//fat arrow anonymouse functions limit scope, eg what is 'this'
+//object destructuring, props is an object passed in, using {courses} pulls that from the object
+//so coutses is the object being worked rather than refe to props.courses
+const CoursesList = ({courses}) => {
+    return courses.map(course => {
+        return (
+            <div key={course.id}>
                     <span><strong>{course.title}</strong> :  </span>
                     <span>By: {course.author}</span>
                     <p />
@@ -19,6 +20,6 @@ function CoursesList(props) {
                 </div>
             );
         })
-    )
+    
 }
 export default CoursesList;
