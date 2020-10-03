b7e866f8af8bff6c802118870f64ee1e0434a759
Graham LOgan <graham@GRAHAMs-MacBook-Air.local>
2020-10-03 12:05:11 +0100

    hope to build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Route, Switch} from 'react-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
